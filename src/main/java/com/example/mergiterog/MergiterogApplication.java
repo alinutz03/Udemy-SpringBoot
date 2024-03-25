3e94222368ba0989809c193a75a44b76ed9ab334
alinutz310 <popescu.alina@79@yahoo.com>
2024-03-25 16:04:23 +0200

    Add Profiles

--- src/main/java/com/example/mergiterog/MergiterogApplication.java
+++ src/main/java/com/example/mergiterog/MergiterogApplication.java
@@ -11,25 +11,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Spring Boot Blog App REST APIs",
-                description = "Spring Boot Blog App REST APIs Documentation",
-                version = "v1.0," ,
-                contact = @Contact(
-                        name = "Alina",
-                        email = "alinaelena1347@gmail.com",
-                        url= "ceva url"
-                ),
-                license = @License(
-                        name = "Apache 2.0",
-                        url = "ceva url 2"
-                )
-        ),
-        externalDocs = @ExternalDocumentation(
-                description =" Spring Boot Blog Application"
-        )
-)
+//@OpenAPIDefinition(
+//        info = @Info(
+//                title = "Spring Boot Blog App REST APIs",
+//                description = "Spring Boot Blog App REST APIs Documentation",
+//                version = "v1.0," ,
+//                contact = @Contact(
+//                        name = "Alina",
+//                        email = "alinaelena1347@gmail.com",
+//                        url= "ceva url"
+//                ),
+//                license = @License(
+//                        name = "Apache 2.0",
+//                        url = "ceva url 2"
+//                )
+//        ),
+//        externalDocs = @ExternalDocumentation(
+//                description =" Spring Boot Blog Application"
+//        )
+//)
 public class MergiterogApplication {
 
     @Bean
